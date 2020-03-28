cbbac04092eb0f3ea652cd90f0b93fa7e64c4805
Kelemen Bea <kelemenbeu@gmail.com>
2020-03-28 16:26:18 +0100

    install dependencies, remove css, test

---
+++ src/App/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="jumbotron">
+            <div className="container">
+                <div className="col-md-8 offset-md-2">
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export { App };
