30ccf8b61955cf467150f613bc1d6124151a5e49
Kelemen Bea <kelemenbeu@gmail.com>
2020-03-30 12:07:17 +0200

    style

--- src/layouts/Framed/Header/index.jsx
+++ src/layouts/Framed/Header/index.jsx
@@ -4,7 +4,7 @@ import { ReactComponent as GrapeLogo } from "../../../grape_logo.svg";
 
 const Header = ({ userName }) => (
   <nav className="navbar navbar-light bg-light">
-    <Link className="navbar-brand" to="/">
+    <Link to="/">
       <GrapeLogo style={{ height: "5rem", width: "auto" }} />
     </Link>
     <h3 className="navbar-text">Beer Finder</h3>
