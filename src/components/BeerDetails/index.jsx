e299082ed68e0a38e4d366e3cb3fe8ef49a28632
Kelemen Bea <kelemenbeu@gmail.com>
2020-03-30 09:31:11 +0200

    BeerDetails style

--- src/components/BeerDetails/index.jsx
+++ src/components/BeerDetails/index.jsx
@@ -2,17 +2,27 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const BeerDetails = ({ data }) => (
-  <div className="row justify-content-md-center">
-    <div className="media" style={{ border: "none", minHeight: "10rem" }}>
+  <div className="row justify-content-md-right">
+    <div
+      className="card"
+      style={{ border: "none", minHeight: "20rem", marginTop: "10%" }}
+    >
       <div className="row no-gutters">
-        <div className="col-md-4">
+        <div className="col-md-3 text-center">
           {data.image_url && (
-            <img src={data.image_url} className="card-img-top" alt="..." />
+            <img
+              src={data.image_url}
+              className="img-fluid"
+              alt="..."
+              style={{ width: "5rem", height: "20rem" }}
+            />
           )}
         </div>
-        <div className="col-md-8 text-center">
+        <div className="col-md-9 text-center">
           <div className="card-body">
             <h5 className="card-title">{data.name}</h5>
+            <h6 className="card-subtitle mb-2 text-muted">{data.tagline}</h6>
+            <p className="card-text">{data.description}</p>
             <p className="card-text">abv: {data.abv}%</p>
             <Link className="btn btn-primary" to="/">
               Back
